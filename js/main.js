d9258840de62462d8044af620a920d2ec91643b4
SitiFatimh <hubgit463@gmail.com>
2025-02-10 23:00:24 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document
+  .querySelector(".js-toggle-mobile-menu")
+  .addEventListener("click", function () {
+    document.querySelector(".header-menu-title").classList.toggle("is-show");
+  });
